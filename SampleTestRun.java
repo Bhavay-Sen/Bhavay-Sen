05031f7b4dd7a0462defb3afb9c30d03fd4dad73
Bhavay-Sen <107627176+Bhavay-Sen@users.noreply.github.com>
2022-09-12 16:44:01 +0530

    Create SampleTestRun.java

---
+++ SampleTestRun.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class SampleTestRun{
+  public static void main(String[] args){
+    System.out.println("This is Sample Test Code just to get familier with interface of GitHub");
+    byte a = 12;
+    byte b = 12;
+    byte sum = a+b;
+    System.out.println(sum);
+  }
+}
